daafaf4a67cec71692441ef92145747bac547fe1
devilsole <ahsanmmu@gmail.com>
2020-11-20 04:00:42 +0800

    update 20nov

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';;
 import { CovidComponent } from './covid/covid.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'covid', component: CovidComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule} from '@angular/material/button';
@@ -36,7 +35,6 @@ const firebaseConfig = {
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
     CovidComponent,
 
   ],
